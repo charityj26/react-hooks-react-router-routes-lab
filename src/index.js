fa1aefff31db1b093596e5b91076364438194292
charity <charity.joseph@students.moringaschool.com>
2023-04-30 16:30:06 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,54 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar"
-import About from "./components/About"
-import Home from "./components/Home"
-import Login from './components/Login';
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
-
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
